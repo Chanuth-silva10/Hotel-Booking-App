0e17777a848182eb9d8507418514dc600329180c
Chanuth-silva10 <chanuthmaduka1986@gmail.com>
2024-09-01 21:26:30 +0530

    implement user and auth routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,21 +1,30 @@
-import express, { Request, Response} from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import userRoutes from "./routes/users";
+import authRoutes from "./routes/auth";
+import cookieParser from "cookie-parser";
+import path from "path";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
 const app = express();
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
-  cors()
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
 );
 
-app.get("/api/test", (req: Request, res: Response) => {
-    res.json({message: "Test api"});
-  });
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
-app.listen(4000, () => {
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+
+app.listen(7000, () => {
   console.log("server running on localhost:7000");
 });
