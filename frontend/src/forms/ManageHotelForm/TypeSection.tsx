cccc350b98da253ae8bea4c2173ac7ddd7a00cf3
Chanuth-silva10 <chanuthmaduka1986@gmail.com>
2024-09-09 16:42:58 +0530

    fix small bugs

--- frontend/src/forms/ManageHotelForm/TypeSection.tsx
+++ frontend/src/forms/ManageHotelForm/TypeSection.tsx
@@ -19,8 +19,8 @@ const TypeSection = () => {
           <label
             className={
               typeWatch === type
-                ? "cursor-pointer bg-blue-300 text-sm rounded-full px-4 py-2 font-semibold"
-                : "cursor-pointer bg-gray-300 text-sm rounded-full px-4 py-2 font-semibold"
+                ? "cursor-pointer bg-customBgLow text-white text-sm rounded-full px-4 py-2 font-semibold"
+                : "cursor-pointer bg-customBgMatch text-white text-sm rounded-full px-4 py-2 font-semibold"
             }
           >
             <input
